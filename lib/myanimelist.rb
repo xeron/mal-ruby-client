1e2573a57bbbbc2b409c33940952100b377640fc
Xeron <xeron.oskom@gmail.com>
2011-06-20 22:59:22 +0400

    Draft for lib and client.

---
+++ lib/myanimelist.rb
@@ -0,0 +1,12 @@
+require 'rubygems'
+require 'open-uri'
+require 'nokogiri'
+
+class MyAnimeList
+
+  def initialize(namespace, action, title, user, password)
+    raise ArgumentError, "Invalid namespase." unless ["anime", "manga"].include? namespace
+    raise ArgumentError, "Invalid action." unless ["search", "add", "update", "delete"].include? action
+  end
+
+end
